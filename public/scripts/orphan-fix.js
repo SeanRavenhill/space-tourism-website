a2cbb7c3d70f55ae19b375d29dc1f0394ee14c3d
SeanRavenhill <seanravenhill@gmail.com>
2021-11-02 23:10:06 +0200

    added original script for my modified orphan-fix.js script, with attribution link in code comments.

--- public/scripts/orphan-fix.js
+++ public/scripts/orphan-fix.js
@@ -7,3 +7,29 @@ let wordCount = text.innerHTML.split(" ").length;
 if (wordCount >= wordsMin) {
 	text.innerHTML = text.innerHTML.replace(/ ([^ ]*)$/, "&nbsp;$1");
 }
+
+
+// Original script from https://alexmansfield.com/javascript/preventing-widows-orphans-with-javascript/
+
+// var nbspSelectors = [
+// 	'h1',
+// 	'h2',
+// 	'h3',
+// 	'h4',
+// 	'h5',
+// 	'h6',
+// 	'p',
+// 	'blockquote'
+// ];
+
+// var nbspWordMin = 6;
+
+// nbspSelectors.forEach(function(selector) {
+// 	var nbspElements = document.querySelectorAll(selector);
+// 	nbspElements.forEach(function(element) {
+// 		var wordCount = (element.innerHTML.split(" ").length);
+// 		if (wordCount >= nbspWordMin ) {
+// 			element.innerHTML = element.innerHTML.replace(/ ([^ ]*)$/,'&nbsp;$1');
+// 		}
+// 	});
+// });
